b552bec7036139182b75c28ed0ec9c2a9c504321
Kelvin Lima <limakelvin530@gmail.com>
2023-10-23 14:29:37 -0300

    Criacao da primeira versao da classe HelloWorld

---
+++ src/controller/HWGIt.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class HWGIt {
+
+	public static void main(String[] args) {
+		
+			System.out.println("Óla Mundo ! ! !");
+			System.out.println("Conhecendo o GIT");
+	}
+
+}
